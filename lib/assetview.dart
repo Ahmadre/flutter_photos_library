c1ab69ecde3789eaf6239070db00995a9fc8b7f1
Ahmadre <rebar.ahmad@telexiom.de>
2019-08-24 21:33:47 +0200

    Fixed unnecessary rebuilds | performance increase

--- lib/assetview.dart
+++ lib/assetview.dart
@@ -2,14 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'asset.dart';
 import 'photos_library.dart';
+import 'dart:typed_data';
 
 class AssetView extends StatefulWidget {
   final int index;
   final Asset asset;
   final double width;
   final double height;
+  Uint8List _imageBytes;
+
   AssetView(
-      {@required this.index, @required this.asset, @required this.width, @required this.height});
+      {@required this.index,
+      @required this.asset,
+      @required this.width,
+      @required this.height});
+
+  Uint8List get getImageBytes => _imageBytes;
 
   @override
   State<StatefulWidget> createState() => AssetState(
@@ -25,11 +33,17 @@ class AssetState extends State<AssetView> {
   final double width;
   final double height;
 
-  ByteData _imageData;
   String _channelName;
 
+  Widget assetWidget = Center(
+      child: SizedBox(
+          width: 32.0, height: 32.0, child: CircularProgressIndicator()));
+
   AssetState(
-      {@required this.index, @required this.asset, @required this.width, @required this.height}) {
+      {@required this.index,
+      @required this.asset,
+      @required this.width,
+      @required this.height}) {
     const prefix = 'flutter.yang.me/photos_library/image';
     this._channelName = prefix + '/' + this.asset.identifier;
   }
@@ -50,8 +64,21 @@ class AssetState extends State<AssetView> {
 
   void _requestThumbnail({int width, int height}) {
     BinaryMessages.setMessageHandler(this._channelName, (message) {
-      this._imageData = message;
+      widget._imageBytes = message.buffer.asUint8List();
       setState(() {});
+      assetWidget = AnimatedSwitcher(
+        duration: const Duration(milliseconds: 2000),
+        child: message != null
+            ? Image.memory(
+                message.buffer.asUint8List(),
+                fit: BoxFit.cover,
+                width: this.width,
+                height: this.height,
+                gaplessPlayback: true,
+              )
+            : Container(),
+        key: ValueKey<ByteData>(message),
+      );
     });
 
     PhotosLibrary.requestThumbnail(this.asset.identifier, width, height);
@@ -59,22 +86,6 @@ class AssetState extends State<AssetView> {
 
   @override
   Widget build(BuildContext context) {
-    if (null != this._imageData) {
-      return AnimatedSwitcher(
-        duration: const Duration(milliseconds: 2000),
-        child: Image.memory(
-          this._imageData.buffer.asUint8List(),
-          fit: BoxFit.cover,
-          width: this.width,
-          height: this.height,
-        ),
-        key: ValueKey<ByteData>(this._imageData),
-      );
-    }
-
-    return Text(
-      '${this.index}',
-      style: Theme.of(context).textTheme.headline,
-    );
+    return assetWidget;
   }
 }
